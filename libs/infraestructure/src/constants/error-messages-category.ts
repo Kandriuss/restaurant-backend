c58025d1edf7264d1cbf2c4531f27bcd91144cdd
Kandriuss <andres.bolivar.sam@gmail.com>
2025-12-06 17:57:14 -0300

    chore: Se agragaron mensajes de error de categories

--- libs/infraestructure/src/constants/error-messages-category.ts
+++ libs/infraestructure/src/constants/error-messages-category.ts
@@ -11,4 +11,6 @@ export const errorMessagesCategory = {
     findByCodeError: (code: string, error: string) => `Error al obtener la categoría con code: ${code}: ${error}`,
     updateSuccess: (id: string) => `Categoría actualizada exitosamente con ID: ${id}`,
     updateError: (id: string, error: string) => `Error al actualizar la categoría con ID: ${id}: ${error}`,
+    deleteSuccess: (id: string) => `Categoría eliminada exitosamente con ID: ${id}`,
+    deleteError: (id: string, error: string) => `Error al eliminar la categoría con ID: ${id}: ${error}`,
 }
